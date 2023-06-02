cf2bafc906c1b67c1f8d73521224e5b365a204e3
omkars8 <97988592+omkars8@users.noreply.github.com>
2023-06-02 11:29:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
-nltk.download('stopwords')
+
 
 # Load data
 news_df = pd.read_csv('train.csv')
